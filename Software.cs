ab67a43bd4c0dec469074b84ce5ac8431ac94c9f
Milan <Milan.Atanasov@ruhr-uni-bochum.de>
2024-01-23 13:45:33 +0100

    new class Software

---
+++ Software.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_PC_Sentinel
+{
+    internal class Software
+    {
+
+        String name;
+        String description;
+        String version;
+
+
+
+
+    }
+}
