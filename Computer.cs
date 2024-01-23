1f65f9e6e0dbab8680b91fe1590a64b725dde8e4
Milan <Milan.Atanasov@ruhr-uni-bochum.de>
2024-01-23 14:07:08 +0100

    Added Class Computer

---
+++ Computer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_PC_Sentinel
+{
+    internal class Computer
+    {
+        String name;
+        ArrayList IPs;
+        String lastTurnedOnOff;
+        ArrayList software = new ArrayList();
+
+        public Computer(String name, ArrayList IPs, String lastTurnedOnOff)
+        {
+            this.name = name;
+            this.IPs = IPs;
+            this.lastTurnedOnOff = lastTurnedOnOff;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public ArrayList getIPs()
+        {
+            return IPs;
+        }
+
+        public String getLastTurnedOnOff()
+        {
+            return lastTurnedOnOff;
+        }
+
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+
+        public void setIPs(ArrayList IPs)
+        {
+            this.IPs = IPs;
+        }
+
+        public void setLastTurnedOnOff(String lastTurnedOnOff)
+        {
+            this.lastTurnedOnOff = lastTurnedOnOff;
+        }
+
+        public override String ToString()
+        {
+            return "Computer{" + "name=" + name + ", IPs=" + IPs + ", lastTurnedOnOff=" + lastTurnedOnOff + '}';
+        }
+
+        public void addSoftware(Software software)
+        {
+            this.software.Add(software);
+        }
+
+    }
+}
--- Software.cs
+++ Software.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,54 @@ namespace Network_PC_Sentinel
         String name;
         String description;
         String version;
+        ArrayList simmularSoftware = new ArrayList();
 
+        public Software(String name, String description, String version)
+        {
+            this.name = name;
+            this.description = description;
+            this.version = version;
+        }   
 
+        public String getName()
+        {
+            return name;
+        }
 
+        public String getDescription()
+        {
+            return description;
+        }
+
+        public String getVersion()
+        {
+            return version;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setDescription(String description)
+        {
+            this.description = description;
+        }
+
+        public void setVersion(String version)
+        {
+            this.version = version;
+        }
+
+        public override String ToString()
+        {
+            return "Software{" + "name=" + name + ", description=" + description + ", version=" + version + '}';
+        }
+
+        private void updateSimmularSoftware()
+        {
+            //TODO
+        }
 
     }
 }
